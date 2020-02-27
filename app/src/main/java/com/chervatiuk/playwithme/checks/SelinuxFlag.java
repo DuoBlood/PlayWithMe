256913aa06fae5f9b893190c5d1d3caff07a02ea
Max Chervatiuk <duo.blood@gmail.com>
2020-02-27 19:18:01 +0200

    Fixed SELinux check

--- app/src/main/java/com/chervatiuk/playwithme/checks/SelinuxFlag.java
+++ app/src/main/java/com/chervatiuk/playwithme/checks/SelinuxFlag.java
@@ -1,5 +1,8 @@
 package com.chervatiuk.playwithme.checks;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.lang.reflect.Method;
 
 public class SelinuxFlag implements Check {
@@ -10,9 +13,24 @@ public class SelinuxFlag implements Check {
       final Class<?> c = Class.forName("android.os.SystemProperties");
       final Method get = c.getMethod("get", String.class);
       final String selinux = (String) get.invoke(c, "ro.build.selinux");
-      return "1".equals(selinux);
+      return "1".equals(selinux) || isSELinuxEnforcing();
     } catch (Exception ignored) {
       return false;
     }
+  }
+
+  private boolean isSELinuxEnforcing() {
+    Boolean enforcing = null;
+    final File f = new File("/sys/fs/selinux/enforce");
+    if (f.exists()) {
+      try (final InputStream is = new FileInputStream("/sys/fs/selinux/enforce")) {
+        enforcing = (is.read() == '1');
+      } catch (Exception ignored) {
+      }
+    }
+    if (enforcing == null) {
+      enforcing = true;
+    }
+    return enforcing;
   }
 }
